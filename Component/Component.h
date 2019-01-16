b877cf7262e891ec81b431ef2df4690d0a7c442f
Artak Tsatinyan <artakts@energizeglobal.com>
2019-01-16 13:55:22 +0400

    Initial commit

---
+++ Component/Component.h
@@ -0,0 +1,19 @@
+//
+//  Component.h
+//  Component
+//
+//  Created by Artak Tsatinyan on 1/16/19.
+//  Copyright © 2019 Artak Tsatinyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Component.
+FOUNDATION_EXPORT double ComponentVersionNumber;
+
+//! Project version string for Component.
+FOUNDATION_EXPORT const unsigned char ComponentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Component/PublicHeader.h>
+
+
---
+++ IosComponents/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IosComponents
+//
+//  Created by Artak Tsatinyan on 1/16/19.
+//  Copyright © 2019 Artak Tsatinyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
